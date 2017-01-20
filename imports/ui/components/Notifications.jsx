76167f77b714ed1112e95e7efcbaf784e1b16e37
sushakov <sergey.ushakov94@gmail.com>
2017-01-20 22:18:55 +0200

    Notification component

--- imports/ui/components/Notifications.jsx
+++ imports/ui/components/Notifications.jsx
@@ -6,14 +6,32 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Notifications } from '/imports/api/notifications/notifications';
 
 class Notification extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {errorMessage: null};
+
+        // This binding is necessary to make `this` work in the callback
+        this.markSeen = this.markSeen.bind(this);
+        this.remove = this.remove.bind(this);
+    }
+
+    markSeen(){
+        Meteor.call('notifications.mark-seen', this.props.item._id);
+    }
+
+    remove(event){
+        Meteor.call('notifications.remove', this.props.item._id);
+    }
 
     render() {
+        let seenLabel = <span className="label label-success">new</span>;
+
         return(
             <li role="menuitem" >
-                <a className="remove-notification pull-right" href="#" data-id="{{_id}}">&times;</a>
-                <a className="notification-item" href="#" data-id="{{_id}}">
+                <a className="notification-item" href={this.props.item.link} onMouseLeave={this.markSeen}>
+                    <span className="remove-notification pull-right" href="#" onClick={this.remove}>&times;</span>
                     <span className="small text-muted">a million years ago</span>
-                    <span className="label label-success" hidden={this.props.item.seen}>new</span>
+                    {this.props.item.seen ? null : seenLabel}
                     <p>{this.props.item.text}</p>
                 </a>
             </li>
@@ -25,10 +43,14 @@ class NotificationsDropdown extends Component{
     getNotifications() {
         let items = [];
         for(let item of this.props.notifications){
-            console.log(item)
-            items.push(<Notification item={item} key={item._id._str}/>);
+            items.push(<Notification item={item} key={item._id}/>);
         }
         return items;
+    }
+
+    dropdownClick(event){
+        console.log(event)
+        event.stopPropagation();
     }
 
     render() {
@@ -39,7 +61,7 @@ class NotificationsDropdown extends Component{
                     <span className="label label-danger">count</span>
                 </a>
 
-                <ul className="dropdown-menu">
+                <ul className="dropdown-menu" onClick={this.dropdownClick}>
                     <h6 className="dropdown-header"><b>Notifications</b></h6>
                     <li className="divider"/>
                     {this.getNotifications()}
@@ -54,5 +76,6 @@ export default createContainer(() => {
     Meteor.subscribe('notifications');
     return {
         notifications: Notifications.find().fetch(),
+        count: Notifications.find().count,
     };
 }, NotificationsDropdown);
