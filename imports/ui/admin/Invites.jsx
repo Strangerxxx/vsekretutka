54d291d0ea1d1ae49550a0f408fa336c8bc5c4da
sushakov <sergey.ushakov94@gmail.com>
2017-01-22 19:23:44 +0200

    created Invites component implemented function set of Invites

---
+++ imports/ui/admin/Invites.jsx
@@ -0,0 +1,76 @@
+import React, { Component, PropTypes} from 'react';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+class InviteRow extends Component {
+    returnStatus(){
+        const invite = this.props.invite;
+        if(invite.status != 'claimed')
+            return invite.status;
+        let user = Meteor.users.findOne({'profile.invite': invite._id});
+        return invite.status + " by " + user.profile.firstName + ' ' + user.profile.lastName;
+    }
+
+    render() {
+        const invite = this.props.invite;
+        const removeInvite = () => {Meteor.call('invites.delete', Meteor.userId(), invite._id)};
+        return(
+            <tr>
+                <td>{invite.createdAt.toLocaleString()}</td>
+                <CopyToClipboard text={Meteor.absoluteUrl() + 'acceptInvite/' + invite._id}>
+                    <td><a href="#">Click to copy</a></td>
+                </CopyToClipboard>
+                <td>{this.returnStatus()}</td>
+                <td><a href="#" onClick={removeInvite}>Delete</a></td>
+            </tr>
+        )
+    }
+}
+
+class InvitesTable extends Component {
+    createInvite(event){
+        event.preventDefault();
+        Meteor.call('invites.create', Meteor.userId());
+    }
+
+    fillInvitesTable(){
+        let inviteRows = [];
+        for(let invite of this.props.invites){
+            inviteRows.push(
+                <InviteRow invite={invite} key={invite._id}/>
+            )
+        }
+        return(inviteRows);
+    }
+    render() {
+        return(
+            <div className="invites">
+                <form onSubmit={this.createInvite}>
+                    <button type="submit" id="create-invite" className="btn btn-primary">Create Invitation</button>
+                </form>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Created At</th>
+                        <th>Registration Link</th>
+                        <th>Status</th>
+                        <th>Delete</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.fillInvitesTable()}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default createContainer(() => {
+    Meteor.subscribe('invites', Meteor.userId());
+    Meteor.subscribe('users', Meteor.userId());
+    return{
+        invites: Invites.find().fetch(),
+    }
+}, InvitesTable);
--- imports/ui/admin/Users.jsx
+++ imports/ui/admin/Users.jsx
@@ -1,60 +1,14 @@
 import React, { Component, PropTypes} from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
-
-class InviteRow extends Component {
-
-    render() {
-        let invite = this.props.invite;
-        return(
-            <tr>
-                <td>{invite.createdAt.toLocaleString()}</td>
-                <td><a href="#" data-clipboard-text="{{url}}{{_id}}">Click to copy</a></td>
-                <td>{invite.status}</td>
-                <td><a href="#">Delete</a></td>
-            </tr>
-        )
-    }
-}
+import InvitesTable from './Invites';
 
 class Users extends Component {
-    createInvite(event){
-        event.preventDefault();
-        Meteor.call('invites.create', Meteor.userId());
-    }
-
-    fillInvitesTable(){
-        let inviteRows = [];
-        for(let invite of this.props.invites){
-            inviteRows.push(
-                <InviteRow invite={invite} key={invite._id}/>
-            )
-        }
-        return(inviteRows);
-    }
-
     render(){
         return(
-            <div className="users" onSubmit={this.createInvite}>
+            <div className="users">
                 <legend>Invitations</legend>
-                <form>
-                    <button type="submit" id="create-invite" className="btn btn-primary">Create Invitation</button>
-                </form>
-                <div id="table-invites">
-                    <table className="table">
-                        <thead>
-                        <tr>
-                            <th>Created At</th>
-                            <th>Registration Link</th>
-                            <th>Status</th>
-                            <th>Delete</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.fillInvitesTable()}
-                        </tbody>
-                    </table>
-                </div>
+                <InvitesTable/>
             </div>
         )
     }
@@ -62,9 +16,7 @@ class Users extends Component {
 
 export default createContainer(() => {
     Meteor.subscribe('users', Meteor.userId());
-    Meteor.subscribe('invites', Meteor.userId());
     return{
         users: Meteor.users.find().fetch(),
-        invites: Invites.find().fetch(),
     }
 }, Users);
