16944398a1817b9fc764b3615b894a04469b378c
sushakov <sergey.ushakov94@gmail.com>
2017-01-29 17:30:27 +0200

    created methods for actions

--- imports/api/progress/actions.js
+++ imports/api/progress/actions.js
@@ -15,16 +15,59 @@ if(Meteor.isServer) {
     });
 
     Meteor.methods({
-        'actions.attach': (userId, adminUserId, MainTaskId) => {
-           if(Roles.userIsInRole(adminUserId, 'admin'))
-               Actions.insert({
-                   userId,
-                   adminUserId,
-                   MainTaskId,
-                   type: 'attach',
-               })
+        'actions.attach': (userId, adminUserId, mainTaskId) => {
+           if(Roles.userIsInRole(adminUserId, 'admin')){
+               if(!Actions.findOne(userId, mainTaskId) || (Actions.findOne({userId, mainTaskId, type: 'attach'}) && Actions.findOne({userId, mainTaskId, type: 'deattach'}) ) )
+                   Actions.insert({
+                       userId,
+                       adminUserId,
+                       mainTaskId,
+                       type: 'attach',
+                   });
+               else{
+                   let user = Meteor.users.findOne(userId);
+                   throw new Meteor.Error('User ' + user.profile.firstName + ' ' + user.profile.lastName + ' is already attached to this task');
+               }
+           }
         },
-        'actions.result': (userId, taskId, )
+        'actions.result': (userId, mainTaskId, adminUserId, subTaskId, result) => {
+            Actions.insert({
+                userId,
+                mainTaskId,
+                adminUserId,
+                subTaskId,
+                result,
+                type: 'result'
+            });
+        },
+        'actions.return': (userId, mainTaskId, adminUserId, subTaskId, message) => {
+            Actions.insert({
+                userId,
+                mainTaskId,
+                adminUserId,
+                subTaskId,
+                message,
+                type: 'return'
+            });
+        },
+        'actions.deattach': (userId, mainTaskId, adminUserId) => {
+            Actions.insert({
+                userId,
+                mainTaskId,
+                adminUserId,
+                type: 'deattach',
+            })
+        },
+        'actions.continue': (userId, mainTaskId, adminUserId, subTaskId, message) => {
+            Actions.insert({
+                userId,
+                mainTaskId,
+                adminUserId,
+                subTaskId,
+                message,
+                type: 'continue'
+            })
+        }
 
     });
 }
@@ -42,7 +85,7 @@ Actions.schema = new SimpleSchema({
     adminUserId: {
         type: Meteor.users,
     },
-    MainTaskId: {
+    mainTaskId: {
         type: Tasks,
     },
     type: {
@@ -63,6 +106,10 @@ Actions.schema = new SimpleSchema({
         type: Tasks,
         optional: true,
     },
+    result: {
+        type: Object,
+        optional: true,
+    }
 });
 
 Actions.attachSchema(Actions.schema);
